0266f865eb2c7ab43a524f9afe7a7f1d1489b233
Rafael <rafaelguanciale@hotmail.com>
2026-07-27 13:12:43 -0300

    Refactor: Gitignore e endpoint da API alteradas

--- permana-blog/src/utils/api.ts
+++ permana-blog/src/utils/api.ts
@@ -1,6 +1,6 @@
 import type { PostPreview } from "./types";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const getPosts = (): Promise<PostPreview[]> => {
   return fetch(`${BASE_URL}/posts`, {
